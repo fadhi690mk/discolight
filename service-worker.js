7af4d7e8e8949b3142bf8d542e26532ee438b094
fadhi690mk <88191826+fadhi690mk@users.noreply.github.com>
2023-10-07 13:27:14 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'color-changer-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png', // Update with the path to your app icon
+  // Add other static assets or pages that should be cached
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => {
+          return name !== CACHE_NAME;
+        }).map((name) => {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
